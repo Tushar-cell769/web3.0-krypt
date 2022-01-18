ae5fdcf1cffa95b3dfc812bb7581b91ccc489185
Tushar Raj <rajtushar33@gmail.com>
2022-01-19 01:36:50 +0530

    Changes for showing transactions.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
-import { AiFillCiCircle } from "react-icons/ai";
+import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
-import Loader from "./Loader";
 import { TransactionContext } from "../context/TransactionContext";
 import { shortenAddress } from "../utils/shortenAddress";
+import { Loader } from ".";
 
 const companyCommonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -23,16 +23,17 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 
 const Welcome = () => {
   const {
-    connectWallet,
     currentAccount,
-    formData,
-    sendTransaction,
+    connectWallet,
     handleChange,
+    sendTransaction,
+    formData,
     isLoading,
   } = useContext(TransactionContext);
 
   const handleSubmit = (e) => {
     const { addressTo, amount, keyword, message } = formData;
+
     e.preventDefault();
 
     if (!addressTo || !amount || !keyword || !message) return;
@@ -43,8 +44,8 @@ const Welcome = () => {
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col mf:mr-10">
-          <h1 className="text-white text-3xl sm:text-5xl text-gradient py-1">
+        <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
@@ -55,8 +56,9 @@ const Welcome = () => {
             <button
               type="button"
               onClick={connectWallet}
-              className="flex flex-row justify-center items-center my-5 bg-[#2962e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
             >
+              <AiFillPlayCircle className="text-white mr-2" />
               <p className="text-white text-base font-semibold">
                 Connect Wallet
               </p>
@@ -67,21 +69,22 @@ const Welcome = () => {
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
               Reliability
             </div>
-            <div className={`${companyCommonStyles}`}>Security</div>
-            <div className={`rounded-tr-2xl ${companyCommonStyles}`}>
+            <div className={companyCommonStyles}>Security</div>
+            <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
               Ethereum
             </div>
-            <div className={`rounded-bl-2xl ${companyCommonStyles}`}>
+            <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
               Web 3.0
             </div>
-            <div className={`${companyCommonStyles}`}>Low fees</div>
+            <div className={companyCommonStyles}>Low Fees</div>
             <div className={`rounded-br-2xl ${companyCommonStyles}`}>
               Blockchain
             </div>
           </div>
         </div>
+
         <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
@@ -109,11 +112,11 @@ const Welcome = () => {
             <Input
               placeholder="Amount (ETH)"
               name="amount"
-              type="amount"
+              type="number"
               handleChange={handleChange}
             />
             <Input
-              placeholder="Keyword (GIF)"
+              placeholder="Keyword (Gif)"
               name="keyword"
               type="text"
               handleChange={handleChange}
@@ -124,16 +127,18 @@ const Welcome = () => {
               type="text"
               handleChange={handleChange}
             />
+
             <div className="h-[1px] w-full bg-gray-400 my-2" />
+
             {isLoading ? (
               <Loader />
             ) : (
               <button
                 type="button"
-                onClick={(e) => handleSubmit(e)}
-                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
               >
-                Send Now
+                Send now
               </button>
             )}
           </div>
--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
-const API_KEY = import.meta.env.VITE_GIPHY_API;
+const APIKEY = import.meta.env.VITE_GIPHY_API;
+
 const useFetch = ({ keyword }) => {
   const [gifUrl, setGifUrl] = useState("");
 
@@ -11,8 +12,9 @@ const useFetch = ({ keyword }) => {
           .split(" ")
           .join("")}&limit=1`
       );
-      const data = await response.json();
-      setGifUrl(data[0]?.image?.downsized_medium?.url);
+      const { data } = await response.json();
+
+      setGifUrl(data[0]?.images?.downsized_medium.url);
     } catch (error) {
       setGifUrl(
         "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284"
@@ -21,9 +23,7 @@ const useFetch = ({ keyword }) => {
   };
 
   useEffect(() => {
-    if (keyword) {
-      fetchGifs();
-    }
+    if (keyword) fetchGifs();
   }, [keyword]);
 
   return gifUrl;
